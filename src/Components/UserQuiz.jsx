b9f897fa815f334caeae99db1d2af5dc6daad857
dermansultan <abdurrehmansultan4@gmail.com>
2020-11-22 02:50:20 -0500

    UserQuiz Page added

--- src/Components/UserQuiz.jsx
+++ src/Components/UserQuiz.jsx
@@ -1,47 +1,66 @@
 import React from "react";
+import badBot from "../Assets/Img/Badbot.png";
+import GoodBot from "../Assets/Img/GoodBot.png";
+import GreatBot from "../Assets/Img/GreatBot.png";
+import UnsureBot from "../Assets/Img/UnsureBot.png";
+import headBot from "../Assets/Img/headBot.png"
+import JournalEntry from "./journalEntry"
 
-class UserQuiz extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
+class UserQuiz extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    // bind methods
+  }
 
-        }
-        // bind methods
-    }
+  // define methods
 
-    // define methods
-
-    render(){
-        return (
-            <div className='containerMain UserQuiz'>
-                <div className='left UserQuiz'>
-                <div className='userQuizContentLeft'>
-                <img alt='HapplyTheBot'/>
+  render() {
+    return (
+      <div className="containerMain UserQuiz">
+        <div className="left UserQuiz">
+          <div className="userQuizContentLeft">
+            <img className='bot quizBanner' alt="HapplyTheBot" src={headBot} />
+          </div>
+        </div>
+        <div className="right UserQuiz">
+          <div className="userQuizContentRight">
+          <JournalEntry></JournalEntry>
+            <div className="feelingWrapper" style={{display:"none"}}>
+              <h1 className="subheader center">How are you feeling today?</h1>
+              <div className="feelingIconWrapper">
+                <div className="botIconFeelWrapper">
+                  <img className="feelingIcon" src={GoodBot}></img>
+                  <p className='bodyText black'>Good</p>
                 </div>
+                <div className="botIconFeelWrapper">
+                  <img className="feelingIcon" src={GreatBot}></img>
+                  <p className='bodyText black'>Great</p>
                 </div>
-                <div className='right UserQuiz'>
-                <div className='userQuizContentRight'>
-                <div className='feelingWrapper'>
-                <h1 className='subheader'>How are you feeling today?</h1>
-                <div className='feelingIconWrapper'>
-                <h1>test</h1>
-                <h1>test</h1>
-                <h1>test</h1>
+                <div className="botIconFeelWrapper">
+                  <img className="feelingIcon" src={badBot}></img>
+                  <p className='bodyText black'>Bad</p>
                 </div>
-                <div className="journalWrapper">
-                <h1 className='subheader'>Would you like to journal today?</h1>
-                <div className='journalChoiceBtnWrapper'>
-                    <button>Yes</button>
-                    <button>No</button>
+                <div className="botIconFeelWrapper">
+                  <img className="feelingIcon" src={UnsureBot}></img>
+                  <p className='bodyText black'>Unsure</p>
                 </div>
+              </div>
+              <div className="journalWrapper">
+                <h1 className="subheader center">
+                  Would you like to journal today?
+                </h1>
+                <div className="journalChoiceBtnWrapper">
+                  <button className='btn journalChoice'>Yes</button>
+                  <button className='btn journalChoice'>No</button>
                 </div>
-                </div>
-                </div>
-                </div>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-export default UserQuiz
+export default UserQuiz;
---
+++ src/Components/journalEntry.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class JournalEntry extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+        // bind methods
+    }
+    // define methods
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default JournalEntry
