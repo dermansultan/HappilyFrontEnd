b4011cb510d97b16d6bed4890f420a1ad9630937
dermansultan <abdurrehmansultan4@gmail.com>
2020-11-22 03:05:36 -0500

    journalForm Component

--- src/Components/journalEntry.jsx
+++ src/Components/journalEntry.jsx
@@ -3,16 +3,33 @@ import React from "react";
 class JournalEntry extends React.Component{
     constructor(props){
         super(props);
-        this.state = {};
-        // bind methods
-    }
-    // define methods
+        this.state = {value: ''};
 
-    render() {
-        return(
-            <div></div>
-        )
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+      }
+    
+      render() {
+        return (
+            <div className='journalFormContainer'>
+                <h1 className='subheader center'>Great! Tell me about it.</h1>
+                <form className='journalForm' onSubmit={this.handleSubmit} id='journalForm'>
+                <label>Reset Journal Entry</label>
+                <textarea className='journalInput' type="text" form='journalForm' value={this.state.value} onChange={this.handleChange} />
+                </form>
+            </div>
+        );
+      }
     }
-}
+
 
 export default JournalEntry
